8c6f033b0c67dd35f11416a401e7321a6a0c2b53
KevZ1209 <43306846+KevZ1209@users.noreply.github.com>
2022-04-13 13:15:12 -0700

    add title screen, update tetris board, add LED

--- tetris.cpp
+++ tetris.cpp
@@ -19,10 +19,103 @@ class OledDisplay
     void drawBoard();
     void drawSquare(char x, char y);
     void clearSquare(char x, char y);
+    void drawSmallSquare(char x, char y);
+    void drawP(char x, char y) {
+      drawSmallSquare(x, y);
+      drawSmallSquare(x + 1, y);
+      drawSmallSquare(x + 2, y);
+      drawSmallSquare(x + 3, y + 1);
+      drawSmallSquare(x + 2, y + 2);
+      drawSmallSquare(x + 1, y + 2);
+      drawSmallSquare(x, y + 2);
+      drawSmallSquare(x, y + 1);
+      drawSmallSquare(x, y + 3);
+      drawSmallSquare(x, y + 4);
+    }
+    void drawR(char x, char y) {
+      drawSmallSquare(x, y);
+      drawSmallSquare(x + 1, y);
+      drawSmallSquare(x + 2, y);
+      drawSmallSquare(x + 3, y + 1);
+      drawSmallSquare(x + 2, y + 2);
+      drawSmallSquare(x + 1, y + 2);
+      drawSmallSquare(x, y + 2);
+      drawSmallSquare(x, y + 1);
+      drawSmallSquare(x, y + 3);
+      drawSmallSquare(x, y + 4);
+      drawSmallSquare(x, y + 4);
+      drawSmallSquare(x + 2, y + 3);
+      drawSmallSquare(x + 3, y + 4);
+    }
+    void drawE(char x, char y) {
+      drawSmallSquare(x, y);
+      drawSmallSquare(x + 1, y);
+      drawSmallSquare(x + 2, y);
+      drawSmallSquare(x + 3, y);
+      drawSmallSquare(x, y + 1);
+      drawSmallSquare(x, y + 2);
+      drawSmallSquare(x + 1, y + 2);
+      drawSmallSquare(x, y + 3);
+      drawSmallSquare(x, y + 4);
+      drawSmallSquare(x + 1, y + 4);
+      drawSmallSquare(x + 2, y + 4);
+      drawSmallSquare(x + 3, y + 4);
+    }
+    void drawS(char x, char y) {
+      drawSmallSquare(x + 1, y);
+      drawSmallSquare(x + 2, y);
+      drawSmallSquare(x + 3, y);
+      drawSmallSquare(x, y + 1);
+      drawSmallSquare(x + 1, y + 2);
+      drawSmallSquare(x + 2, y + 2);
+      drawSmallSquare(x + 3, y + 3);
+      drawSmallSquare(x, y + 4);
+      drawSmallSquare(x + 1, y + 4);
+      drawSmallSquare(x + 2, y + 4);
+    }
+
+    void drawO(char x, char y) {
+      drawSmallSquare(x + 1, y);
+      drawSmallSquare(x + 2, y);
+      drawSmallSquare(x, y + 1);
+      drawSmallSquare(x + 3, y + 1);
+      drawSmallSquare(x, y + 2);
+      drawSmallSquare(x + 3, y + 2);
+      drawSmallSquare(x, y + 3);
+      drawSmallSquare(x + 3, y + 3);
+      drawSmallSquare(x + 1, y + 4);
+      drawSmallSquare(x + 2, y + 4);
+    }
+
+    void drawA(char x, char y) {
+      drawSmallSquare(x + 1, y);
+      drawSmallSquare(x + 2, y);
+      drawSmallSquare(x, y + 1);
+      drawSmallSquare(x + 3, y + 1);
+      drawSmallSquare(x, y + 2);
+      drawSmallSquare(x + 1, y + 2);
+      drawSmallSquare(x + 2, y + 2);
+      drawSmallSquare(x + 3, y + 2);
+      drawSmallSquare(x, y + 3);
+      drawSmallSquare(x + 3, y + 3);
+      drawSmallSquare(x, y + 4);
+      drawSmallSquare(x + 3, y + 4);
+    }
+
+    void drawT(char x, char y) {
+      drawSmallSquare(x, y);
+      drawSmallSquare(x + 1, y);
+      drawSmallSquare(x + 2, y);
+      drawSmallSquare(x + 3, y);
+      drawSmallSquare(x + 4, y);
+      drawSmallSquare(x + 2, y + 1);
+      drawSmallSquare(x + 2, y + 2);
+      drawSmallSquare(x + 2, y + 3);
+      drawSmallSquare(x + 2, y + 4);
+    }
     void render() {
       m_display.display();
     }
-
     void clearScreen();
     void flashScreen();
   private:
@@ -51,6 +144,14 @@ void OledDisplay::begin(uint8_t switchvcc, uint8_t i2caddr)
 void OledDisplay::drawBoard()
 {
   m_display.drawRect(3, 1, 123, 63, white);
+  for (byte row = 1; row < 20; row++) {
+    for (byte col = 1; col < 10; col++) {
+      if (row % 2 == 0 && col % 2 == 0) {
+        m_display.drawPixel(row * 6 + 4, col * 6 + 2, white);
+      }
+
+    }
+  }
   m_display.display();
 }
 
@@ -64,7 +165,13 @@ void OledDisplay::drawSquare(char x, char y)
 void OledDisplay::clearSquare(char x, char y) {
   char newx = 4 + 6 * y;
   char newy = 56 - 6 * x;
-  m_display.fillRect(newx, newy, 6, 6, black);
+  m_display.fillRect(newx + 1, newy + 1, 5, 5, black);
+}
+
+void OledDisplay::drawSmallSquare(char x, char y) {
+  char newx = 4 + 2 * y;
+  char newy = 56 - 2 * x;
+  m_display.fillRect(newx, newy, 2, 2, white);
 }
 
 //Clears the entire screen
@@ -164,7 +271,7 @@ bool l_piece[16] = {0, 0, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 bool s_piece[16] = {0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-bool t_piece[16] = {1, 1, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+bool t_piece[16] = {0, 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 bool z_piece[16] = {1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
@@ -405,18 +512,59 @@ int tempo = 500;
 
 void setup() {
   // put your setup code here, to run once:
-  display.begin();
-  randomSeed(analogRead(A2));
-  Serial.begin(9600);
-  display.drawBoard();
-  delay(500);
   pinMode(VRX, INPUT);
   pinMode(VRY, INPUT);
   pinMode(SW, INPUT_PULLUP);
+  pinMode(3, OUTPUT);
+  display.begin();
+  start_time = millis();
+  
+  randomSeed(analogRead(A2));
+  
+  display.render();
+  while (getJoyStickInput() != 4) {
+    if (millis() - start_time >= 500) {
+      display.drawP(0, 15);
+      display.drawR(5, 15);
+      display.drawE(10, 15);
+      display.drawS(15, 15);
+      display.drawS(20, 15);
+      display.render();
+    }
+
+    if (millis() - start_time >= 1000) {
+      display.drawT(0, 21);
+      display.drawO(5, 21);
+      display.render();
+    }
+
+    if (millis() - start_time >= 1500) {
+      display.drawS(0, 27);
+      display.drawT(5, 27);
+      display.drawA(10, 27);
+      display.drawR(15, 27);
+      display.drawT(20, 27);
+      display.render();
+    }
+    
+    if (millis() - start_time >= 3000) {
+      display.clearScreen();
+      start_time = millis();
+    }
+    
+  }
+  display.clearScreen();
+
+  // get a random number so that we don't start with the same piece
+  random(0, 7);
+  
+  Serial.begin(9600);
+  display.drawBoard();
+  delay(500);
 
   display.render();
 
-  start_time = millis();
+
 
 }
 
@@ -425,17 +573,22 @@ void setup() {
 Tetromino* tet = nullptr;
 
 void loop() {
+
   tet = new Tetromino(&tb, random(0, 7));
   if (tet->canMoveDown() == false) {
     display.flashScreen();
     display.clearScreen();
     Serial.print(actualScore);
     Serial.print('\n');
-
+    for (int c = 0; c < actualScore; c++) {
+      delay(200);
+      digitalWrite(3, HIGH);
+      delay(200);
+      digitalWrite(3, LOW);
+    }
     exit(0);
   }
   while (tet->canMoveDown()) {
-
     if (millis() - start_time >= tempo) {
       tet->shiftDown();
       start_time = millis();
